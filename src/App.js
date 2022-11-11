4b2a8899e135c7ae3956c631738eff1283e4cd00
floatingnahid22 <nahid.uap.github@gmail.com>
2022-11-12 00:17:12 +0600

    crud app complete

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
-<<<<<<< HEAD
 import "./App.css";
 import Index from "./routes";
-=======
-import CounterView from "./components/counterView";
->>>>>>> 2e147f031e9ac6db7e7164255d9b043fd7bc9e57
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
       <Index />
-=======
-      <CounterView />
->>>>>>> 2e147f031e9ac6db7e7164255d9b043fd7bc9e57
     </div>
   );
 }
--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,4 @@
 import { configureStore } from "@reduxjs/toolkit";
-<<<<<<< HEAD
 import booksReducer from "../features/books/BookSlice";
 
 const store = configureStore({
@@ -8,14 +7,4 @@ const store = configureStore({
   },
 });
 
-=======
-
-import counterReducer from "../features/counter/counterSlice";
-
-const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
-});
->>>>>>> 2e147f031e9ac6db7e7164255d9b043fd7bc9e57
 export default store;
